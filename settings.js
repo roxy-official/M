1169d55eb2cba785aff72356a2bed1dbe49d7b49
Roxy-official <89264186+roxy-official@users.noreply.github.com>
2023-08-06 12:12:56 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,27 +21,27 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
+global.owner = ['94716315866'] //ur owner number
+global.ownernomer = "94755228896" //ur owner number2
+global.ownername = "👻 Malik Abhishek" //ur owner name
+global.ytname = "YT: Music Nooo" //ur yt chanel name
+global.socialm = "GitHub: roxy-official" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //new
-global.botname = "Cheems Bot MD V8"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["916909137213@s.whatsapp.net"]
-global.ownerweb = "https://youtube.com/@DGXeon"
+global.botname = "© ʀᴏx⃟ - ᴮᴼᵀ 🇱🇰 "
+global.ownernumber = '👻 Malik Abhishek'
+global.ownername = '♡_______♡'
+global.ownerNumber = ["94716315866@s.whatsapp.net"]
+global.ownerweb = "https://youtube.com/@MusicNooo"
 global.websitex = "https://youtu.be/L_SIk59QeAU"
 global.wagc = "https://chat.whatsapp.com/Dc2qyVeK8JbJq8Gr3U1pKH"
-global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
+global.themeemoji = '👻'
+global.wm = "Rox Bot Inc."
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
 global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n"
-global.creator = "916909137213@s.whatsapp.net"
+global.author = "♡____♡\n\n"
+global.creator = "94716315866@s.whatsapp.net"
 global.prefa = ['','!','.','#','&']
 global.sessionName = 'session'
 global.hituet = 0
